cedbfe79bc3af9fbc2728b2fc66b01e4cb1169c8
CJ42 <cavallerajean@gmail.com>
2023-10-27 10:12:52 +0100

    build: add optimiser settings to compile contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,40 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from 'hardhat/config';
 import { NetworkUserConfig } from 'hardhat/types';
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
 
-import * as dotenv from "dotenv"
-dotenv.config()
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 function getTestnetChainConfig(): NetworkUserConfig {
-    const config: NetworkUserConfig = {
-      url: 'https://rpc.testnet.lukso.network',
-      chainId: 4201,
-    };
-  
-    if (process.env.PRIVATE_KEY !== undefined) {
-      config['accounts'] = [process.env.PRIVATE_KEY];
-    }
-  
-    return config;
+  const config: NetworkUserConfig = {
+    url: 'https://rpc.testnet.lukso.network',
+    chainId: 4201,
+  };
+
+  if (process.env.PRIVATE_KEY !== undefined) {
+    config['accounts'] = [process.env.PRIVATE_KEY];
   }
 
+  return config;
+}
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: '0.8.19',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   typechain: {
     outDir: 'typechain-types',
     target: 'ethers-v6',
   },
   networks: {
-    luksoTestnet: getTestnetChainConfig()
-  }
+    luksoTestnet: getTestnetChainConfig(),
+  },
 };
 
 export default config;
