4c2de17d4b325228b96bce4fa18d6f00269bf0e4
CJ42 <cavallerajean@gmail.com>
2023-11-18 01:31:29 +0300

    chore: add examples for LSP recipes

--- scripts/mintTickets.ts
+++ scripts/mintTickets.ts
@@ -1,33 +1,28 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
-import {
-    EventTicketsNFT,
-    EventTicketsNFT__factory
-} from "../typechain-types";
+import { EventTicketsNFT, EventTicketsNFT__factory } from '../typechain-types';
 
 async function deployAndCreateTickets() {
-    const accounts = await ethers.getSigners();
-    const deployer = accounts[0];
+  const accounts = await ethers.getSigners();
+  const deployer = accounts[0];
 
-    const luksoMeetupTickets: EventTicketsNFT = await new EventTicketsNFT__factory(
-        deployer
-    ).deploy(
-        "LUKSO Meetup #2",
-        "MUP2",
-        deployer.address,
-    )
+  const luksoMeetupTickets: EventTicketsNFT = await new EventTicketsNFT__factory(deployer).deploy(
+    'LUKSO Meetup #2',
+    'MUP2',
+    deployer.address,
+  );
 
-    // create 100 entry tickets.
-    // Give them to the deployer initially, who will distribute them afterwards.
-    await luksoMeetupTickets.mint(
-        deployer.address, // recipient
-        100, // amount
-        true, // force sending to an EOA
-        "minting 100 tickets" // data
-    );
+  // create 100 entry tickets.
+  // Give them to the deployer initially, who will distribute them afterwards.
+  await luksoMeetupTickets.mint(
+    deployer.address, // recipient
+    100, // amount
+    true, // force sending to an EOA
+    ethers.encodeBytes32String('minting 100 tickets'), // data
+  );
 }
 
 deployAndCreateTickets().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-  });
+  console.error(error);
+  process.exitCode = 1;
+});
